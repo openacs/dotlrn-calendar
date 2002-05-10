e6d55eaaebc08f2ccc8ef54bcab94eb502aeb3c0
Arjun Sanyal <arjun@openforce.net>
2002-05-10 06:35:14 +0000

    Added 3 procs to applet contract and impls. Infrastructure for getting rid of dummy_comm_p, cloning, and community deleting

--- sql/oracle/dotlrn-calendar-create.sql
+++ sql/oracle/dotlrn-calendar-create.sql
@@ -113,6 +113,33 @@ begin
 	       'TCL'
 	);
 
+    -- AddPortlet
+    foo := acs_sc_impl.new_alias (
+        impl_contract_name => 'dotlrn_applet',
+        impl_name => 'dotlrn_calendar',
+        impl_operation_name => 'AddPortlet',
+        impl_alias => 'dotlrn_calendar::add_portlet',
+        impl_pl => 'TCL'
+    );
+
+    -- RemovePortlet
+    foo := acs_sc_impl.new_alias (
+        impl_contract_name => 'dotlrn_applet',
+        impl_name => 'dotlrn_calendar',
+        impl_operation_name => 'RemovePortlet',
+        impl_alias => 'dotlrn_calendar::remove_portlet',
+        impl_pl => 'TCL'
+    );
+
+    -- Clone
+    foo := acs_sc_impl.new_alias (
+        impl_contract_name => 'dotlrn_applet',
+        impl_name => 'dotlrn_calendar',
+        impl_operation_name => 'Clone',
+        impl_alias => 'dotlrn_calendar::clone',
+        impl_pl => 'TCL'
+    );
+
 	-- Add the binding
 	acs_sc_binding.new (
 	    contract_name => 'dotlrn_applet',
