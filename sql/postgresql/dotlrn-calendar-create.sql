f63d4e93cb9b0c0f4ae73b1ba6f1d946a673c089
Gustaf Neumann <gustaf.neumann@wu-wien.ac.at>
2014-08-18 21:10:51 +0000

    - modernize SQL (dollar quoting)

--- sql/postgresql/dotlrn-calendar-create.sql
+++ sql/postgresql/dotlrn-calendar-create.sql
@@ -24,142 +24,149 @@
 -- $Id$
 --
 
-create function inline_0()
-returns integer as '
-declare
+
+
+--
+-- procedure inline_0/0
+--
+CREATE OR REPLACE FUNCTION inline_0(
+
+) RETURNS integer AS $$
+DECLARE
 	foo integer;
-begin
+BEGIN
 	-- create the implementation
 	foo := acs_sc_impl__new (
-		''dotlrn_applet'',
-		''dotlrn_calendar'',
-		''dotlrn_calendar''
+		'dotlrn_applet',
+		'dotlrn_calendar',
+		'dotlrn_calendar'
 	);
 
 	-- GetPrettyName
 	foo := acs_sc_impl_alias__new (
-	       ''dotlrn_applet'',
-	       ''dotlrn_calendar'',
-	       ''GetPrettyName'',
-	       ''dotlrn_calendar::get_pretty_name'',
-	       ''TCL''
+	       'dotlrn_applet',
+	       'dotlrn_calendar',
+	       'GetPrettyName',
+	       'dotlrn_calendar::get_pretty_name',
+	       'TCL'
 	);
 
 	-- AddApplet
 	foo := acs_sc_impl_alias__new (
-	       ''dotlrn_applet'',
-	       ''dotlrn_calendar'',
-	       ''AddApplet'',
-	       ''dotlrn_calendar::add_applet'',
-	       ''TCL''
+	       'dotlrn_applet',
+	       'dotlrn_calendar',
+	       'AddApplet',
+	       'dotlrn_calendar::add_applet',
+	       'TCL'
 	);
 
 	-- RemoveApplet
 	foo := acs_sc_impl_alias__new (
-	       ''dotlrn_applet'',
-	       ''dotlrn_calendar'',
-	       ''RemoveApplet'',
-	       ''dotlrn_calendar::remove_applet'',
-	       ''TCL''
+	       'dotlrn_applet',
+	       'dotlrn_calendar',
+	       'RemoveApplet',
+	       'dotlrn_calendar::remove_applet',
+	       'TCL'
 	);
 
 	-- AddAppletToCommunity
 	foo := acs_sc_impl_alias__new (
-	       ''dotlrn_applet'',
-	       ''dotlrn_calendar'',
-	       ''AddAppletToCommunity'',
-	       ''dotlrn_calendar::add_applet_to_community'',
-	       ''TCL''
+	       'dotlrn_applet',
+	       'dotlrn_calendar',
+	       'AddAppletToCommunity',
+	       'dotlrn_calendar::add_applet_to_community',
+	       'TCL'
 	);
 
 	-- RemoveAppletFromCommunity
 	foo := acs_sc_impl_alias__new (
-	       ''dotlrn_applet'',
-	       ''dotlrn_calendar'',
-	       ''RemoveAppletFromCommunity'',
-	       ''dotlrn_calendar::remove_applet_from_community'',
-	       ''TCL''
+	       'dotlrn_applet',
+	       'dotlrn_calendar',
+	       'RemoveAppletFromCommunity',
+	       'dotlrn_calendar::remove_applet_from_community',
+	       'TCL'
 	);
 
 	-- AddUser
 	foo := acs_sc_impl_alias__new (
-	       ''dotlrn_applet'',
-	       ''dotlrn_calendar'',
-	       ''AddUser'',
-	       ''dotlrn_calendar::add_user'',
-	       ''TCL''
+	       'dotlrn_applet',
+	       'dotlrn_calendar',
+	       'AddUser',
+	       'dotlrn_calendar::add_user',
+	       'TCL'
 	);
 
 	-- RemoveUser
 	foo := acs_sc_impl_alias__new (
-	       ''dotlrn_applet'',
-	       ''dotlrn_calendar'',
-	       ''RemoveUser'',
-	       ''dotlrn_calendar::remove_user'',
-	       ''TCL''
+	       'dotlrn_applet',
+	       'dotlrn_calendar',
+	       'RemoveUser',
+	       'dotlrn_calendar::remove_user',
+	       'TCL'
 	);
 
 	-- AddUserToCommunity
 	foo := acs_sc_impl_alias__new (
-	       ''dotlrn_applet'',
-	       ''dotlrn_calendar'',
-	       ''AddUserToCommunity'',
-	       ''dotlrn_calendar::add_user_to_community'',
-	       ''TCL''
+	       'dotlrn_applet',
+	       'dotlrn_calendar',
+	       'AddUserToCommunity',
+	       'dotlrn_calendar::add_user_to_community',
+	       'TCL'
 	);
 
 	-- RemoveUserFromCommunity
 	foo := acs_sc_impl_alias__new (
-	       ''dotlrn_applet'',
-	       ''dotlrn_calendar'',
-	       ''RemoveUserFromCommunity'',
-	       ''dotlrn_calendar::remove_user_from_community'',
-	       ''TCL''
+	       'dotlrn_applet',
+	       'dotlrn_calendar',
+	       'RemoveUserFromCommunity',
+	       'dotlrn_calendar::remove_user_from_community',
+	       'TCL'
 	);
 
     -- AddPortlet
     foo := acs_sc_impl_alias__new (
-        ''dotlrn_applet'',
-        ''dotlrn_calendar'',
-        ''AddPortlet'',
-        ''dotlrn_calendar::add_portlet'',
-        ''TCL''
+        'dotlrn_applet',
+        'dotlrn_calendar',
+        'AddPortlet',
+        'dotlrn_calendar::add_portlet',
+        'TCL'
     );
 
     -- RemovePortlet
     foo := acs_sc_impl_alias__new (
-        ''dotlrn_applet'',
-        ''dotlrn_calendar'',
-        ''RemovePortlet'',
-        ''dotlrn_calendar::remove_portlet'',
-        ''TCL''
+        'dotlrn_applet',
+        'dotlrn_calendar',
+        'RemovePortlet',
+        'dotlrn_calendar::remove_portlet',
+        'TCL'
     );
 
     -- Clone
     foo := acs_sc_impl_alias__new (
-        ''dotlrn_applet'',
-        ''dotlrn_calendar'',
-        ''Clone'',
-        ''dotlrn_calendar::clone'',
-        ''TCL''
+        'dotlrn_applet',
+        'dotlrn_calendar',
+        'Clone',
+        'dotlrn_calendar::clone',
+        'TCL'
     );
 
     foo := acs_sc_impl_alias__new (
-        ''dotlrn_applet'',
-        ''dotlrn_calendar'',
-        ''ChangeEventHandler'',
-        ''dotlrn_calendar::change_event_handler'',
-        ''TCL''
+        'dotlrn_applet',
+        'dotlrn_calendar',
+        'ChangeEventHandler',
+        'dotlrn_calendar::change_event_handler',
+        'TCL'
     );
 
 	-- Add the binding
 	perform acs_sc_binding__new (
-	    ''dotlrn_applet'',
-	    ''dotlrn_calendar''
+	    'dotlrn_applet',
+	    'dotlrn_calendar'
 	);
 
 	return 0;
 
-end;' language 'plpgsql';
+END;
+$$ LANGUAGE plpgsql;
 select inline_0();
 drop function inline_0();
