fbbd1181326e3afae8afaf82c60f8f9ed70e3997
Hector Romojaro <hector.romojaro@gmail.com>
2019-09-05 13:17:48 +0000

    Add a small testsuite for the 'dotlrn-calendar' package

---
+++ tcl/test/dotlrn-calendar-test-procs.tcl
@@ -0,0 +1,33 @@
+ad_library {
+
+        Automated tests for the dotlrn-calendar package.
+
+        @author Héctor Romojaro <hector.romojaro@gmail.com>
+        @creation-date 2019-09-05
+
+}
+
+aa_register_case \
+    -cats {api smoke production_safe} \
+    -procs {
+        dotlrn_calendar::package_key
+        dotlrn_calendar::my_package_key
+        dotlrn_calendar::applet_key
+    } \
+    dotlrn_calendar__keys {
+
+        Simple test for the various dotlrn_calendar::..._key procs.
+
+        @author Héctor Romojaro <hector.romojaro@gmail.com>
+        @creation-date 2019-09-05
+} {
+    aa_equals "Package key" "[dotlrn_calendar::package_key]" "calendar"
+    aa_equals "My Package key" "[dotlrn_calendar::my_package_key]" "dotlrn_calendar"
+    aa_equals "Applet key" "[dotlrn_calendar::applet_key]" "dotlrn_calendar"
+}
+
+# Local variables:
+#    mode: tcl
+#    tcl-indent-level: 4
+#    indent-tabs-mode: nil
+# End:
